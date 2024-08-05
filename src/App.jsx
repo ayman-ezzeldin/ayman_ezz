51224241b97088d7404ea357589a72c0e7f2f2b3
Ayman ezz <aymanmae12@gmail.com>
2024-08-05 22:59:35 +0300

    back Default

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary ">
-        <div className="">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center mb-24 ">
           <Navbar />
           <Hero />
         </div >
